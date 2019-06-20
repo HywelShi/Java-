b6865237e28c818b0a0c26b2424112d4fc75ddf3
Hywel <1095623912@qq.com>
2019-06-20 15:12:28 +0800

    Add files via upload

--- TrainingCamp/src/main/java/java基础/day01/Random.java
+++ TrainingCamp/src/main/java/java基础/day01/Random.java
@@ -3,7 +3,12 @@ package java基础.day01;
 public class Random {
 
 	public static void main(String[] args) {
-		// 
+		// 随机数公式
+		// 生成a-b之间的随机整数 要求a与b为整数且a<b
+		// (int)(Math.random()*(b-a+1)+a)
+		// 生成1-100之间的随机整数
+		int num = (int)(Math.random()*100+1);
+		System.out.println(num);
 
 	}
 
