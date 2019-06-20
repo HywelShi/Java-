f933aba26c5cad62e20f9ea596f25fa9274b71ed
Hywel <1095623912@qq.com>
2019-06-20 15:04:02 +0800

    开营
    搭建基本训练营课程模块

---
+++ TrainingCamp/src/main/java/java基础/day01/HelloWorld.java
@@ -0,0 +1,8 @@
+package java基础.day01;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//syso ALt+? 回车
+		System.out.println("HelloWorld");
+	}
+}
---
+++ TrainingCamp/src/main/java/java基础/day01/Random.java
@@ -0,0 +1,10 @@
+package java基础.day01;
+
+public class Random {
+
+	public static void main(String[] args) {
+		// 
+
+	}
+
+}
